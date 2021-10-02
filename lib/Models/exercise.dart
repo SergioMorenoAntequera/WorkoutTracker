082e7acda67989ec24f02ac85f3900689e857bb1
SergioMorenoAntequera <seranmoreno500@gmail.com>
2021-10-02 14:13:09 +0200

    preparing  models

---
+++ lib/Models/exercise.dart
@@ -0,0 +1,10 @@
+class Exercise {
+  var start = DateTime(2021);
+  var end = DateTime(2021);
+  var duration = 0; // Millis
+
+  var type = 0;
+  var muscles = 0;
+  var participants = "";
+  var description = "";
+}
